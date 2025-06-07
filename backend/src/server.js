0ebe535c37f394b99c4ea8640ece7a0d82d99d40
drezix <andre.t.agostinis@gmail.com>
2025-06-07 00:41:57 -0300

    refactor(middleware): extrair a lógica de formatação de resposta e geração de links para módulo auxiliar

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,6 +9,19 @@ app.use(express.json());
 /*----------------------INSERT----------------------*/
 app.use('/news', newsRoutes);
 
+app.use((err, req, res, next) => {
+    console.error('ERRO GLOBAL:', err.stack);
+  
+    const errorMessage = process.env.NODE_ENV === 'production' 
+      ? 'Ocorreu um erro interno no servidor.' 
+      : err.message;
+      
+    res.status(err.status || 500).json({
+      message: 'Algo deu errado!',
+      error: errorMessage,
+    });
+  });
+
 connectDB();
 
 module.exports = app;
