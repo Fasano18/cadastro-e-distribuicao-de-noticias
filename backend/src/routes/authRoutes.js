2b56f4d37e252d40d117d13995ed1fae6ba8f7e0
Leonardo Fasano <fasano@MacBook-Air-de-Leonardo.local>
2025-06-30 20:24:11 -0300

    add routes

---
+++ backend/src/routes/authRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const authController = require('../controllers/authController');
+const { authMiddleware } = require('../middlewares/authMiddleware');
+const { checkLoginAttempts, incrementLoginAttempts } = require('../middlewares/loginAttemptMiddleware');
+const router = express.Router();
+
+// POST /auth/register
+router.post('/register', authController.register);
+router.post('/login', checkLoginAttempts, incrementLoginAttempts, authController.login);
+router.post('/logout', authMiddleware, authController.logout);
+
+module.exports = router;
--- backend/src/routes/newsRoutes.js
+++ backend/src/routes/newsRoutes.js
@@ -2,16 +2,19 @@ const newsController = require('../controllers/newsControllers');
 const express = require('express');
 const router = express.Router();
 const responseFormatter = require('../middlewares/responseFormatter');
+const {
+    optionalAuthMiddleware, 
+    authMiddleware,
+    adminMiddleware
+} = require('../middlewares/authMiddleware');
 
+// Protected routes for admins
+router.post('/', authMiddleware, adminMiddleware, newsController.insert, responseFormatter);
+router.put('/:id', authMiddleware, adminMiddleware, newsController.update, responseFormatter);
+router.delete('/:id', authMiddleware, adminMiddleware, newsController.remove, responseFormatter);
 
-router.post('/', newsController.insert, responseFormatter);
+router.get('/:id', optionalAuthMiddleware, newsController.getById, responseFormatter);
+router.get('/', optionalAuthMiddleware, newsController.getAll, responseFormatter);
 
-router.get('/:id', newsController.getById, responseFormatter);
-
-router.get('/', newsController.getAll, responseFormatter);
-
-router.put('/:id', newsController.update, responseFormatter);
-
-router.delete('/:id', newsController.remove, responseFormatter);
 
 module.exports = router;
