a518fcaa4ab96764edc0f9da9c135d59793b5ece
drezix <andre.t.agostinis@gmail.com>
2025-07-02 20:12:30 -0300

    feat(db): adicionar usuário padrão admin ao iniciar a aplicação

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,12 +1,45 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const bcrypt = require('bcrypt');
+const User = require('../models/userModel');
 
 dotenv.config();
+
+const createDefaultAdmin = async () => {
+  try {
+    const existingAdmin = await User.findOne({
+      $or: [{ username: 'admin' }, { email: 'admin@gmail.com' }],
+    });
+
+    if (!existingAdmin) {
+      console.log('Nenhum usuário administrador padrão encontrado. Criando um...');
+      
+      const hashedPassword = await bcrypt.hash('admin', 10);
+
+      const adminUser = new User({
+        username: 'admin',
+        email: 'admin@gmail.com',
+        password: hashedPassword,
+        isAdmin: true,
+      });
+
+      await adminUser.save();
+      console.log('Usuário administrador padrão criado com sucesso!');
+      console.log('Username: admin | Senha: admin');
+    } else {
+      console.log('Usuário administrador padrão já existe no banco de dados.');
+    }
+  } catch (error) {
+    console.error('Erro ao tentar criar o usuário administrador padrão:', error);
+  }
+};
 
 const connectDB = async () => {
   try{
     await mongoose.connect(process.env.MONGO_URI);
-      console.log('Connected to the database');
+    console.log('Connected to the database');
+
+    await createDefaultAdmin();
   } catch (error) {
     console.log('Error connecting to the database', error);
     process.exit(1);
